651dc276b83bb872d3a14e244b3423d60f176e31
TommasoGalistu <tommasogalistu92@gmail.com>
2024-05-07 18:00:10 +0200

    aggiunta funzionalità js al pulsante mute

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+let pulsanteUnmute = document.querySelector('.fa-microphone')
+
+
+pulsanteUnmute.addEventListener('click', () => {
+    pulsanteUnmute.classList.remove('fa-microphone')
+    pulsanteUnmute.classList.add('fa-microphone-slash')
+})
+
+
+
+
