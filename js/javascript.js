af3928e312329707bd35b7d245d7c01dc3e87b1a
TommasoGalistu <tommasogalistu92@gmail.com>
2024-05-07 18:05:05 +0200

    fine prima funzionalità unmute mute

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,13 @@
 let pulsanteUnmute = document.querySelector('.fa-microphone')
+let testoMicrUnmute = document.querySelector('.testoMicr')
 
 
 pulsanteUnmute.addEventListener('click', () => {
     pulsanteUnmute.classList.remove('fa-microphone')
     pulsanteUnmute.classList.add('fa-microphone-slash')
+
+    testoMicrUnmute.textContent = 'Mute'
+    
 })
 
 
