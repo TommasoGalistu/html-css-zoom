1718243ac5fdb6254f5860fece3a2483e4bc9e2a
TommasoGalistu <tommasogalistu92@gmail.com>
2024-05-08 11:13:23 +0200

    aggiunta funzionalità tasto microfono completo

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,17 @@
-let pulsanteUnmute = document.querySelector('.fa-microphone')
-let testoMicrUnmute = document.querySelector('.testoMicr')
-
+let pulsanteUnmute = document.querySelector('.fa-microphone');
+let testoMicrUnmute = document.querySelector('.testoMicr');
 
 pulsanteUnmute.addEventListener('click', () => {
-    pulsanteUnmute.classList.remove('fa-microphone')
-    pulsanteUnmute.classList.add('fa-microphone-slash')
-
-    testoMicrUnmute.textContent = 'Mute'
-    
-})
+    if (pulsanteUnmute.classList.contains('fa-microphone')) {
+        pulsanteUnmute.classList.remove('fa-microphone');
+        pulsanteUnmute.classList.add('fa-microphone-slash');
+        testoMicrUnmute.textContent = 'Mute';
+    } else {
+        pulsanteUnmute.classList.remove('fa-microphone-slash');
+        pulsanteUnmute.classList.add('fa-microphone');
+        testoMicrUnmute.textContent = 'Unmute';
+    }
+});
 
 
 
